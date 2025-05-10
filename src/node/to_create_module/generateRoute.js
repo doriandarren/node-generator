3c6fba3275aa77f2b79073724d907c2d717cdb11
mile <darimile@gmail.com>
2025-05-10 10:59:19 +0200

    fix changes

--- src/node/to_create_module/generateRoute.js
+++ src/node/to_create_module/generateRoute.js
@@ -3,7 +3,7 @@ import path from 'path';
 import { createFolder } from '../../helpers/helperFile.js';
 
 
-export const generateRoute = async(fullPath, namespace, nameProject, projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularNameCase, pluralNameCase, columns) => {    
+export const generateRoute = async(fullPath, namespace, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularNameCase, pluralNameCase, columns) => {    
 
     // Folder
     const folderPath = path.join(fullPath, 'src', 'routes', namespace);
