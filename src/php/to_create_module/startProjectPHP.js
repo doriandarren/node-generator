732b575020b32d8d9094c39f89e466e00bc4c454
Dorian Gonzalez <doriandarren1@gmail.com>
2025-05-16 22:33:13 +0200

    fix nodeJS

--- src/php/to_create_module/startProjectPHP.js
+++ src/php/to_create_module/startProjectPHP.js
@@ -1,7 +1,28 @@
+import { pause } from "../../helpers/inquirer";
 
 
 export const startProjectPHP = async() => {
     
-    console.log("Start Project PHP");
+    // Ruta predeterminada
+        const defaultPath = "/Users/dorian/NodejsProjects";
+    
+        const projectName = await readInput("Nombre del proyecto Node: ");
+        let projectPath = await readInput(`Ruta para crear el proyecto (por defecto: ${defaultPath}): `, true);
+    
+        // Si no se introduce una ruta, usar la predeterminada
+        if( !projectPath){
+            projectPath = defaultPath;
+        }
+            
+    
+        // Combinar la ruta y el nombre del proyecto
+        const fullPath = `${projectPath}/${projectName}`;
+
+
+
+        console.log(fullPath);
+
+
+        await pause();
 
 }
