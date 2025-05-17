bd48dd15018604f20ead5cd362113d01fea98ce0
Dorian Gonzalez <doriandarren1@gmail.com>
2025-05-17 23:17:14 +0200

    ad generateBatchProcesses

--- src/php/to_create_module/startProjectPHP.js
+++ src/php/to_create_module/startProjectPHP.js
@@ -1,5 +1,8 @@
 import { readInput } from "../../helpers/inquirer.js";
+import { generateBatchProcesses } from "../to_create_project/generateBatchProcesses.js";
+import { generateEnums } from "../to_create_project/generateEnums.js";
 import { generateFpdfMerge } from "../to_create_project/generateFpdfMerge.js";
+import { generateMaatwebsiteExcel } from "../to_create_project/generateMaatwebsiteExcel.js";
 import { generatePHPCommandLine } from "../to_create_project/generatePHPCommandLine.js";
 import { generateSnappy } from "../to_create_project/generateSnappy.js";
 
@@ -8,27 +11,32 @@ import { generateSnappy } from "../to_create_project/generateSnappy.js";
 export const startProjectPHP = async() => {
     
     // Ruta predeterminada
-        const defaultPath = "/Users/dorian/PhpstormProjects81";
+    const defaultPath = "/Users/dorian/PhpstormProjects81";
+
+    const projectName = await readInput("Nombre del proyecto Node: ");
+    let projectPath = await readInput(`Ruta para crear el proyecto (por defecto: ${defaultPath}): `, true);
+
+    // Si no se introduce una ruta, usar la predeterminada
+    if( !projectPath){
+        projectPath = defaultPath;
+    }
     
-        const projectName = await readInput("Nombre del proyecto Node: ");
-        let projectPath = await readInput(`Ruta para crear el proyecto (por defecto: ${defaultPath}): `, true);
-    
-        // Si no se introduce una ruta, usar la predeterminada
-        if( !projectPath){
-            projectPath = defaultPath;
-        }
-        
-    
-        // Combinar la ruta y el nombre del proyecto
-        const fullPath = `${projectPath}/${projectName}`;
+
+    // Combinar la ruta y el nombre del proyecto
+    const fullPath = `${projectPath}/${projectName}`;
+
+    await generatePHPCommandLine(fullPath);
+    await generateSnappy(fullPath);
+    await generateFpdfMerge(fullPath);
+    await generateMaatwebsiteExcel(fullPath);
+    await generateEnums(fullPath);
+
+    await generateBatchProcesses(fullPath);
 
 
 
-        await generatePHPCommandLine(fullPath);
-        await generateSnappy(fullPath);
-        await generateFpdfMerge(fullPath);
 
-        
+
 
 }
 
---
+++ src/php/to_create_project/generateMaatwebsiteExcel.js
@@ -0,0 +1,22 @@
+import { runExec } from '../../helpers/helperFile.js';
+import { printMessage } from '../../helpers/inquirer.js';
+
+export const generateMaatwebsiteExcel = async (fullPath) => {
+  await installExcel(fullPath);
+  await publishExcel(fullPath);
+};
+
+const installExcel = async (fullPath) => {
+  printMessage('Instalando Excel...', 'cyan');
+  await runExec('composer require maatwebsite/excel', fullPath);
+  printMessage('Excel instalado correctamente.', 'green');
+};
+
+const publishExcel = async (fullPath) => {
+  printMessage('Publicando Excel...', 'cyan');
+  await runExec(
+    'php artisan vendor:publish --provider="Maatwebsite\\Excel\\ExcelServiceProvider"',
+    fullPath
+  );
+  printMessage('Excel publicado correctamente.', 'green');
+};
