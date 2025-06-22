f4ab001bab9745c6169ee23e12bc26160302ed99
Dorian Gonzalez <doriandarren1@gmail.com>
2025-06-22 21:29:37 +0200

    fix stander

--- src/controllers/react/to_create_module/generateModuleStandardReact.js
+++ src/controllers/react/to_create_module/generateModuleStandardReact.js
@@ -35,15 +35,15 @@ export const generateModuleStandardReact = async (
   // }
 
   // if (selectedComponents.includes("list") || selectedComponents.includes("list")) {
-  //   await generateList(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularFirstCamel, columns);
+  //   await generateList(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularNameCamel, columns);
   // }
 
-  // if (selectedComponents.includes("create") || selectedComponents.includes("create")) {
-  //   await generateCreate(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularFirstCamel, columns);
-  // }
+  if (selectedComponents.includes("create") || selectedComponents.includes("create")) {
+    await generateCreate(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularFirstCamel, columns);
+  }
 
   if (selectedComponents.includes("edit") || selectedComponents.includes("edit")) {
-    await generateEdit(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularFirstCamel, columns);
+    await generateEdit(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularNameCamel, columns);
   }
 
   if (selectedComponents.includes("barrel")) {
@@ -51,7 +51,7 @@ export const generateModuleStandardReact = async (
   }
 
   if (selectedComponents.includes("service")) {
-    await generateService(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularFirstCamel, columns);
+    await generateService(projectPath, singularName, pluralName, singularNameKebab, pluralNameKebab, singularNameSnake, pluralNameSnake, singularNameCamel, columns);
   }
 
 };
