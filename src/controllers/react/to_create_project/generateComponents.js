9cd528449c03d1d6bf06bbb57d7ff5956a6c5e53
Dorian Gonzalez <doriandarren1@gmail.com>
2025-07-11 09:07:08 +0200

    add bg-warning ComponentButton

--- src/controllers/react/to_create_project/generateComponents.js
+++ src/controllers/react/to_create_project/generateComponents.js
@@ -64,12 +64,13 @@ export const Button = ({ children, type = "button", variant = "primary", onClick
       className={classNames(
         "py-2 px-4 w-full xl:w-32 xl:mr-3 rounded-md text-white font-semibold transition-all duration-200 cursor-pointer",
         {
-          "bg-primary hover:bg-primary-dark": !disabled && variant === "primary",
           "bg-gray-400 cursor-not-allowed": disabled,
+          "bg-primary hover:bg-primary-dark": !disabled && variant === "primary",
           "bg-danger hover:bg-danger-dark": !disabled && variant === "danger",
           "bg-secondary hover:bg-secondary-dark": !disabled && variant === "secondary",
           "bg-success hover:bg-success-dark": !disabled && variant === "success",
           "bg-info hover:bg-info-dark": !disabled && variant === "info",
+          "bg-warning hover:bg-warning-dark": !disabled && variant === "warning",
         },
         className
       )}
