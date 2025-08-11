107ecc45f30a4c61c806c4cf5c00e00419a552c4
Dorian Gonzalez <doriandarren1@gmail.com>
2025-08-11 12:59:12 +0200

    add init php

--- src/controllers/php/to_create_project/dev/generateTestController.js
+++ src/controllers/php/to_create_project/dev/generateTestController.js
@@ -63,7 +63,14 @@ class TestController extends Controller
 
     public function __invokeDISCORR()
     {
+
+        // ini_set('memory_limit', '-1');
+        // ini_set('max_execution_time', '0');
+        // set_time_limit(0);
+
+
         MessageChannel::send('Hola Mundo!!!!', 'Titulo');
+        
     }
 
 
