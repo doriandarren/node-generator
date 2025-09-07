ad581919aafe50b75e801016abb82f6a47a0797d
Dorian Gonzalez <doriandarren1@gmail.com>
2025-09-07 13:03:24 +0200

    fix toggle

--- generator/controllers/react/to_create_project/generate_components/generateToggle.js
+++ generator/controllers/react/to_create_project/generate_components/generateToggle.js
@@ -28,20 +28,20 @@ export const ThemedToggle = ({
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={\`group relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-hidden ${
+        className={\`group relative inline-flex h-6 w-11 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-hidden \${
           enabled ? 'bg-indigo-600' : 'bg-gray-200'
         }\`}
       >
         <span className="sr-only">{label ?? 'Toggle setting'}</span>
         <span
-          className={\`pointer-events-none relative inline-block size-5 transform rounded-full bg-white ring-0 shadow-sm transition duration-200 ease-in-out ${
+          className={\`pointer-events-none relative inline-block size-5 transform rounded-full bg-white ring-0 shadow-sm transition duration-200 ease-in-out \${
             enabled ? 'translate-x-5' : 'translate-x-0'
           }\`}
         >
           {/* Iconos */}
           <span
             aria-hidden="true"
-            className={\`absolute inset-0 flex size-full items-center justify-center transition-opacity duration-200 ease-in ${
+            className={\`absolute inset-0 flex size-full items-center justify-center transition-opacity duration-200 ease-in \${
               enabled ? 'opacity-0 duration-100 ease-out' : 'opacity-100'
             }\`}
           >
@@ -57,7 +57,7 @@ export const ThemedToggle = ({
           </span>
           <span
             aria-hidden="true"
-            className={\`absolute inset-0 flex size-full items-center justify-center transition-opacity duration-200 ease-in ${
+            className={\`absolute inset-0 flex size-full items-center justify-center transition-opacity duration-200 ease-in \${
               enabled ? 'opacity-100' : 'opacity-0'
             }\`}
           >
