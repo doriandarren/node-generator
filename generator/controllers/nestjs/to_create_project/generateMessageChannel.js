bdc8ec95473cb96eb9e5ff0e6ade017f464cf3dd
Dorian Gonzalez <doriandarren1@gmail.com>
2025-11-19 20:18:01 +0100

    fix name env

--- generator/controllers/nestjs/to_create_project/generateMessageChannel.js
+++ generator/controllers/nestjs/to_create_project/generateMessageChannel.js
@@ -1,17 +1,19 @@
 import fs from "fs";
 import path from "path";
 import { createFolder } from "../../../helpers/helperFile.js";
-import { addHeaderLine, addModuleExport, addModuleProvider } from "../helpers/helperFileWrite.js";
+import {
+  addHeaderLine,
+  addModuleExport,
+  addModuleProvider,
+} from "../helpers/helperFileWrite.js";
 
 export const generateMessageChannel = async (fullPath) => {
-    await createMessageChannel(fullPath);
+  await createMessageChannel(fullPath);
 
-    await addHeader(fullPath);
-    await addModuleProviderAdapter(fullPath);
-    await addModuleExportAdapter(fullPath);
-
+  await addHeader(fullPath);
+  await addModuleProviderAdapter(fullPath);
+  await addModuleExportAdapter(fullPath);
 };
-
 
 const createMessageChannel = async (fullPath) => {
   // Folder
@@ -37,8 +39,8 @@ export class MessageChannelService {
   /**
    * Send message webhook extern
    */
-  async send(text: string, title = 'Title', isError: false): Promise<void> {
-    const url = this.configService.get<string>('MESSAGE_CHENNEL');
+  async send(text: string, title:string = 'Title', isError:boolean = false): Promise<void> {
+    const url = this.configService.get<string>('MESSAGE_CHANNEL');
     const appName = this.configService.get<string>('APP_NAME');
     const appEnv = this.configService.get<string>('APP_ENV');
 
@@ -82,8 +84,6 @@ export class MessageChannelService {
     console.error(`❌ Error al crear archivo: ${error.message}`);
   }
 };
-
-
 
 const addHeader = async (fullPath) => {
   const filePath = path.join(fullPath, "src", "common", "common.module.ts");
